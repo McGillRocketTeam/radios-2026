4ce0cb8eae633ae522132ce97ddd33cb69c9b791
Steev3n <steven.thao@mail.mcgill.ca>
2026-03-21 13:05:04 -0400

    removed useless radioModule in radiochiptest class

--- GroundStation/include/variant/RadioChipTestVariant.h
+++ GroundStation/include/variant/RadioChipTestVariant.h
@@ -1,14 +1,9 @@
 #pragma once
 #include "IVariant.h"
-#include <memory>
-
-class RadioModule;
 
 class RadioChipTestVariant : public IVariant
 {
 public:
     void setup() override;
     void loop() override;
-private:
-    std::unique_ptr<RadioModule> radioModule;
 };
