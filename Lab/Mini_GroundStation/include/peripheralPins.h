56f707f6dd1e9f88e2e460ac666e537315cb9a50
Jeff <jeffreywjlim@gmail.com>
2026-04-17 20:57:57 -0400

    SPI based OLED now works

--- Lab/Mini_GroundStation/include/peripheralPins.h
+++ Lab/Mini_GroundStation/include/peripheralPins.h
@@ -1,50 +1,30 @@
 #pragma once
 
-// OLD vs NEW
+//Shared SPI
+constexpr int MOSI_PIN = 11;
+constexpr int MISO_PIN = 12;
+constexpr int SCK_PIN = 13;
 
-// Radios
-// constexpr int NSS_PIN = 10;
-// constexpr int DIO1_PIN = 14;
-// constexpr int BUSY_PIN = 15;
-// constexpr int RST_PIN = 18;
-// constexpr int FREQ_PIN = 22;
-// constexpr int RX_LED_PIN = 23;
-// constexpr int TX_LED_PIN = 19;
-
-// Display
-// constexpr int DISPLAY_SDA = 18;
-// constexpr int DISPLAY_SCL = 19;
-// constexpr int DISPLAY_RST = 17;
-
-// Joystick
-// constexpr int STICK_VRX_PIN = 14;
-// constexpr int STICK_VRY_PIN = 15;
-// constexpr int STICK_SW_PIN = 16;
-
-
-// Harmonised New
-
-// Radios untouched
-constexpr int NSS_PIN = 10;
+// Radios Specific SPI pins
+constexpr int NSS_PIN = 10; // NSS is the CS for the Radio
 constexpr int DIO1_PIN = 14;
 constexpr int BUSY_PIN = 15;
 constexpr int RST_PIN = 18;
 constexpr int FREQ_PIN = 22;
 
-// Change the tx/rx leds
-constexpr int RX_LED_PIN = 3;
-constexpr int TX_LED_PIN = 4;
+// OLED Specifc SPI pins
+// Lablling on OLED is misleading
+// SCL → SCK
+// SDA → MOSI
+// CS  → CS
+// DC  → any GPIO
+// RES → any GPIO
+constexpr int DISPLAY_CS_PIN   = 9;
+constexpr int DISPLAY_DC_PIN   = 8;
+constexpr int DISPLAY_RST_PIN  = 7;
 
-// Update to using i2c wire 2
-constexpr int DISPLAY_SDA = 17;
-constexpr int DISPLAY_SCL = 16;
-// Reset doesnt need analog just gpio
-constexpr int DISPLAY_RST = 9;
-
+// Joystick
 constexpr int STICK_VRX_PIN = 19;
 constexpr int STICK_VRY_PIN = 20;
 constexpr int STICK_SW_PIN = 21;
-
-
-
 
